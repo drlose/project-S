4c38db4843105e4113d91bb4ed9bb53b668f3159
drlose <drlose99@hotmail.com>
2020-01-23 11:43:03 +0900

    c

---
+++ New work/Assets/TEST/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    private Transform m_Transform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Transform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
